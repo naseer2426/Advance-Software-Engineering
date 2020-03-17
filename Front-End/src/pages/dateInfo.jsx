29769cfb82759b96be86a54bb5cbaefe99026185
Aditi <aditisaini99@gmail.com>
2020-03-18 01:10:18 +0800

    Updated format for showing attendance

--- Front-End/src/pages/dateInfo.jsx
+++ Front-End/src/pages/dateInfo.jsx
@@ -25,35 +25,66 @@ class DateInfo extends Component {
           Students Gallore
         </h1>
         <div>
+          <h1
+            style={{
+              fontFamily: "Lora",
+              fontSize: "30px",
+              textAlign: "center",
+              fontWeight: "bold"
+            }}
+            className="mt-5"
+          >
+            Students Present
+          </h1>
           {dateData && (
-            <Row>
-              {dateData.Students.Present.map((studentP, index) => (
-                <Col sm="4" className="mt-5" key={index}>
-                  <Card>
-                    <Card.Img variant="top" src={studentP.url} />
-                    <Card.Body>
-                      <Card.Title className="mb-4">{studentP.name}</Card.Title>
-                      <Card.Subtitle className="mb-2 text-muted">
-                        Present
-                      </Card.Subtitle>
-                    </Card.Body>
-                  </Card>
-                </Col>
-              ))}
-              {dateData.Students.Absent.map((studentA, index) => (
-                <Col sm="4" className="mt-5" key={index}>
-                  <Card>
-                    <Card.Img variant="top" src={studentA.url} />
-                    <Card.Body>
-                      <Card.Title className="mb-4">{studentA.name}</Card.Title>
-                      <Card.Subtitle className="mb-2 text-muted">
-                        Absent
-                      </Card.Subtitle>
-                    </Card.Body>
-                  </Card>
-                </Col>
-              ))}
-            </Row>
+            <>
+              <Row>
+                {dateData.Students.Present.map((studentP, index) => (
+                  <Col sm="4" className="mt-5" key={index}>
+                    <Card>
+                      <Card.Img variant="top" src={studentP.url} />
+                      <Card.Body>
+                        <Card.Title className="mb-4">
+                          {studentP.name}
+                        </Card.Title>
+                        {/* <Card.Subtitle className="mb-2 text-muted">
+                          Present
+                        </Card.Subtitle> */}
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                ))}
+              </Row>
+
+              <h1
+                style={{
+                  fontFamily: "Lora",
+                  fontSize: "30px",
+                  textAlign: "center",
+                  fontWeight: "bold"
+                }}
+                className="mt-5"
+              >
+                Students Absent
+              </h1>
+              <Row>
+                {dateData.Students.Absent.map((studentA, index) => (
+                  <Col sm="4" className="mt-5" key={index}>
+                    <Card>
+                      <Card.Img variant="top" src={studentA.url} />
+                      <Card.Body>
+                        <Card.Title className="mb-4">
+                          {studentA.name}
+                        </Card.Title>
+                        {/* <Card.Subtitle className="mb-2 text-muted">
+                          Absent
+                        </Card.Subtitle> */}
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                ))}
+              </Row>
+            </>
           )}{" "}
         </div>
       </>
